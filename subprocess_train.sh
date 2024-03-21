385870bd2f572b1a6e00c9887833c1756495f46c
Alexander Partin <25892264+adpartin@users.noreply.github.com>
2024-03-21 17:48:55 -0500

    Update subprocess_train.sh
    Align code

--- subprocess_train.sh
+++ subprocess_train.sh
@@ -8,4 +8,7 @@ model_outdir=$3
 # activate conda env for model
 source activate /path/to/model/conda/env/
 
-python model_train.py --train_ml_data_dir=$train_ml_data_dir --val_ml_data_dir=$val_ml_data_dir --model_outdir=$model_outdir
+python model_train.py \
+  --train_ml_data_dir=$train_ml_data_dir \
+  --val_ml_data_dir=$val_ml_data_dir \
+  --model_outdir=$model_outdir
